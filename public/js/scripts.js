9162b94090cba5d1f2763bc037a325ebdd7d3d27
miketheburnz <mbyrnes727@gmail.com>
2024-06-15 16:03:15 +0200

    Pull commit

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -7,4 +7,19 @@ $(document).ready(function() {
             scrollTop: $($(this).attr('href')).offset().top
         }, 500, 'linear');
     });
+
+    // Parallax scrolling effect
+    $(window).on('scroll', function() {
+        var scrollPos = $(window).scrollTop();
+        $('.parallax').css('background-position', 'center ' + (scrollPos * 0.5) + 'px');
+
+        // Scroll reveal effect
+        $('.scroll-reveal').each(function() {
+            var revealPos = $(this).offset().top;
+            var windowHeight = $(window).height();
+            if (scrollPos + windowHeight > revealPos) {
+                $(this).addClass('reveal');
+            }
+        });
+    });
 });
